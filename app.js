1472d9c1c4bdbf10ac60f77a7dc15d5826352107
TAHA ANWAR <84726883+tahaanwar2@users.noreply.github.com>
2023-07-31 14:31:38 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,9 @@
+function onbulb(){
+    var bulb = document.getElementById("bulb");
+    bulb.src = "onbulb.jpg"
+}
+
+function offbulb(){
+    var bulb = document.getElementById("bulb");
+    bulb.src = "offbulb.jpg"
+}
